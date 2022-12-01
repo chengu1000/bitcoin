1ad78cc6f057b649fe3c4e6a668b108c2fb06ee1
chengu1000 <chengu1000@naver.com>
2022-12-01 22:53:37 +0900

    five

--- message.py
+++ message.py
@@ -3,10 +3,9 @@ import pyupbit
 import datetime
 import requests
 
-myToken = "xoxb-4348614544996-4331639062583-VjfmGhoPvw4aDV4FhBoAC7kO"
+myToken = "xoxb-4348614544996-4331639062583-5MWdf1vYnbUl4EJdcppuXQ40"
 
 def post_message(token, channel, text):
-    """슬랙 메시지 전송"""
     response = requests.post("https://slack.com/api/chat.postMessage",
         headers={
             "Authorization": "Bearer "+token
@@ -17,4 +16,5 @@ def post_message(token, channel, text):
 
 # 시작 메세지 슬랙 전송
 post_message(myToken,"#bitcoin", "autotrade start")
+print("start")
 
--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import requests
 
 access = "vcJTH06hDw0Hww6p3ovEszogEkNIDdpCO17V7hA4"
 secret = "Rt5mSxXxGYO0JxNgERPPbFZLMOhppxrNxyoI3C5E"
-myToken = "xoxb-4348614544996-4331639062583-VjfmGhoPvw4aDV4FhBoAC7kO"
+myToken = "xoxb-4348614544996-4331639062583-5MWdf1vYnbUl4EJdcppuXQ40"
 
 def post_message(token, channel, text):
     """슬랙 메시지 전송"""
@@ -52,6 +52,7 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 # 시작 메세지 슬랙 전송
 post_message(myToken,"#bitcoin", "autotrade start")
+count = 0
 
 while True:
     try:
@@ -60,19 +61,83 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            ma15 = get_ma15("KRW-BTC")
-            current_price = get_current_price("KRW-BTC")
-            if target_price < current_price and ma15 < current_price:
+            #btc
+            target_price_btc = get_target_price("KRW-BTC", 0.5)
+            ma15_btc = get_ma15("KRW-BTC")
+            current_price_btc = get_current_price("KRW-BTC")
+            if target_price_btc < current_price_btc and ma15_btc < current_price_btc:
                 krw = get_balance("KRW")
+                count = count + 1
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995/(5-count))
                     post_message(myToken,"#bitcoin", "BTC buy : " +str(buy_result))
+            #eth
+            target_price_eth = get_target_price("KRW-ETH", 0.5)
+            ma15_eth = get_ma15("KRW-ETH")
+            current_price_eth = get_current_price("KRW-ETH")
+            if target_price_eth < current_price_eth and ma15_eth < current_price_eth:
+                krw = get_balance("KRW")
+                count = count + 1
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order("KRW-ETH", krw*0.9995/(5-count))
+                    post_message(myToken,"#bitcoin", "ETH buy : " +str(buy_result))
+            #doge
+            target_price_doge = get_target_price("KRW-DOGE", 0.5)
+            ma15_doge = get_ma15("KRW-DOGE")
+            current_price_doge = get_current_price("KRW-DOGE")
+            if target_price_doge < current_price_doge and ma15_doge < current_price_doge:
+                krw = get_balance("KRW")
+                count = count + 1
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order("KRW-DOGE", krw*0.9995/(5-count))
+                    post_message(myToken,"#bitcoin", "DOGE buy : " +str(buy_result))
+            #xrp
+            target_price_XRP = get_target_price("KRW-XRP", 0.5)
+            ma15_XRP = get_ma15("KRW-XRP")
+            current_price_XRP = get_current_price("KRW-XRP")
+            if target_price_XRP < current_price_XRP and ma15_XRP < current_price_XRP:
+                krw = get_balance("KRW")
+                count = count + 1
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order("KRW-XRP", krw*0.9995/(5-count))
+                    post_message(myToken,"#bitcoin", "XRP buy : " +str(buy_result))
+            #ada
+            target_price_ADA = get_target_price("KRW-ADA", 0.5)
+            ma15_ADA = get_ma15("KRW-ADA")
+            current_price_ADA = get_current_price("KRW-ADA")
+            if target_price_ADA < current_price_ADA and ma15_ADA < current_price_ADA:
+                krw = get_balance("KRW")
+                count = count + 1
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order("KRW-ADA", krw*0.9995/(5-count))
+                    post_message(myToken,"#bitcoin", "ADA buy : " +str(buy_result))
         else:
+            count = 0
+            #btc
             btc = get_balance("BTC")
             if btc > 0.00008:
                 sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
                 post_message(myToken,"#bitcoin", "BTC buy : " +str(sell_result))
+            #eth
+            eth = get_balance("ETH")
+            if eth > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-ETH", eth*0.9995)
+                post_message(myToken,"#bitcoin", "ETH buy : " +str(sell_result))
+            #doge
+            doge = get_balance("DOGE")
+            if doge > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-DOGE", doge*0.9995)
+                post_message(myToken,"#bitcoin", "DOGE buy : " +str(sell_result))
+            #xrp
+            xrp = get_balance("XRP")
+            if xrp > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-XRP", xrp*0.9995)
+                post_message(myToken,"#bitcoin", "XRP buy : " +str(sell_result))
+            #ADA
+            ada = get_balance("ADA")
+            if ada > 0.00008:
+                sell_result = upbit.sell_market_order("KRW-ADA", ada*0.9995)
+                post_message(myToken,"#bitcoin", "ADA buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
