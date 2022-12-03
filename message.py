390fb31b87160bbf718dfeb12a607f818664de87
chengu1000 <chengu1000@naver.com>
2022-12-03 20:20:12 +0900

    count fix

--- message.py
+++ message.py
@@ -5,16 +5,6 @@ import requests
 
 myToken = ""
 
-def post_message(token, channel, text):
-    response = requests.post("https://slack.com/api/chat.postMessage",
-        headers={
-            "Authorization": "Bearer "+token
-            },
-        data={"channel": channel,"text": text}
-    )
-
-
-# 시작 메세지 슬랙 전송
-post_message(myToken,"#bitcoin", "autotrade start")
-print("start")
+count = [1,2,3,4,5]
+print(count[0])
 
--- test.py
+++ test.py
@@ -52,7 +52,7 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 # 시작 메세지 슬랙 전송
 post_message(myToken,"#bitcoin", "autotrade start")
-count = 0
+count = [0,0,0,0,0]
 
 while True:
     try:
@@ -65,54 +65,50 @@ while True:
             target_price_btc = get_target_price("KRW-BTC", 0.5)
             ma15_btc = get_ma15("KRW-BTC")
             current_price_btc = get_current_price("KRW-BTC")
-            if target_price_btc < current_price_btc and ma15_btc < current_price_btc:
-                krw = get_balance("KRW")
-                count = count + 1
+            krw = krw = get_balance("KRW")
+            if target_price_btc < current_price_btc and ma15_btc < current_price_btc and count[0] == 0:
+                count[0] = count[0] + 1
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995/(5-count))
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995/(5))
                     post_message(myToken,"#bitcoin", "BTC buy : " +str(buy_result))
             #eth
             target_price_eth = get_target_price("KRW-ETH", 0.5)
             ma15_eth = get_ma15("KRW-ETH")
             current_price_eth = get_current_price("KRW-ETH")
-            if target_price_eth < current_price_eth and ma15_eth < current_price_eth:
-                krw = get_balance("KRW")
-                count = count + 1
+            if target_price_eth < current_price_eth and ma15_eth < current_price_eth and count[1] == 0:
+                count[1] = count[1] + 1
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-ETH", krw*0.9995/(5-count))
+                    buy_result = upbit.buy_market_order("KRW-ETH", krw*0.9995/(5))
                     post_message(myToken,"#bitcoin", "ETH buy : " +str(buy_result))
             #doge
             target_price_doge = get_target_price("KRW-DOGE", 0.5)
             ma15_doge = get_ma15("KRW-DOGE")
             current_price_doge = get_current_price("KRW-DOGE")
-            if target_price_doge < current_price_doge and ma15_doge < current_price_doge:
-                krw = get_balance("KRW")
-                count = count + 1
+            if target_price_doge < current_price_doge and ma15_doge < current_price_doge and count[2] == 0:
+                count[2] = count[2] + 1
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-DOGE", krw*0.9995/(5-count))
+                    buy_result = upbit.buy_market_order("KRW-DOGE", krw*0.9995/(5))
                     post_message(myToken,"#bitcoin", "DOGE buy : " +str(buy_result))
             #xrp
             target_price_XRP = get_target_price("KRW-XRP", 0.5)
             ma15_XRP = get_ma15("KRW-XRP")
             current_price_XRP = get_current_price("KRW-XRP")
-            if target_price_XRP < current_price_XRP and ma15_XRP < current_price_XRP:
-                krw = get_balance("KRW")
-                count = count + 1
+            if target_price_XRP < current_price_XRP and ma15_XRP < current_price_XRP and count[3] == 0:
+                count[3] = count[3] + 1
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-XRP", krw*0.9995/(5-count))
+                    buy_result = upbit.buy_market_order("KRW-XRP", krw*0.9995/(5))
                     post_message(myToken,"#bitcoin", "XRP buy : " +str(buy_result))
             #ada
             target_price_ADA = get_target_price("KRW-ADA", 0.5)
             ma15_ADA = get_ma15("KRW-ADA")
             current_price_ADA = get_current_price("KRW-ADA")
-            if target_price_ADA < current_price_ADA and ma15_ADA < current_price_ADA:
-                krw = get_balance("KRW")
-                count = count + 1
+            if target_price_ADA < current_price_ADA and ma15_ADA < current_price_ADA and count[4] == 0:
+                count[4] = count[4] + 1
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-ADA", krw*0.9995/(5-count))
+                    buy_result = upbit.buy_market_order("KRW-ADA", krw*0.9995/(5))
                     post_message(myToken,"#bitcoin", "ADA buy : " +str(buy_result))
         else:
-            count = 0
+            count = [0,0,0,0,0]
             #btc
             btc = get_balance("BTC")
             if btc > 0.00008:
